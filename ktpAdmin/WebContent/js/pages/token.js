0e42d36ce68bd464d6d07d713b226729c08e20f3
Lee Chan Ho <echan1020@gmail.com>
2016-08-24 15:43:25 +0900

    토큰관리 페이지 변경 !

--- ktpAdmin/WebContent/js/pages/token.js
+++ ktpAdmin/WebContent/js/pages/token.js
@@ -5,239 +5,242 @@ function tokenSearch() {
 	var input_subscribe = $('#input_subscribe').val();
 
 	if (checkForm) {
-		//#statistics-search-select option:selected
-		var iDselectOptionV=$("#token-search-select option:selected").val();
-		if(iDselectOptionV==1){
+		// #statistics-search-select option:selected
+		var iDselectOptionV = $("#token-search-select option:selected").val();
+		if (iDselectOptionV == 1) {
 			$
-			.ajax({
-				url : '/v1/tokenMulti/' + input_subscribe,
-				type : 'GET',
-				headers : {
-					'X-ApiKey' : tokenID
-				},
-				contentType : "application/json",
-				async : false,
-				success : function(data) {
-					var tableData = [];
-					if (data.result.data) {
+					.ajax({
+						url : '/v1/tokenMulti/' + input_subscribe,
+						type : 'GET',
+						headers : {
+							'X-ApiKey' : tokenID
+						},
+						contentType : "application/json",
+						async : false,
+						success : function(data) {
+							var tableData = [];
+							if (data.result.data) {
+								$('#amdin-token-list').show();
+								for ( var i in data.result.data) {
 
-						for ( var i in data.result.data) {
+									var item = data.result.data[i];
+									console.log(item);
+									tableData.push({
+										"TokenID" : item.tokenID,
+										"UserID" : item.userID
+									});
+								}
 
-							var item = data.result.data[i];
-							console.log(item);
-							tableData.push({
-								"TokenID" : item.tokenID,
-								"UserID" : item.userID
-							});
+								// 기존 data 삭제
+
+								if (token_Table != null && token_Table != "") {
+									console.log('testconsole');
+									$('#dataTable_Token').children().remove();
+								}
+
+								token_Table = $('#dataTable_Token').dataTable({
+									bJQueryUI : true,
+									bDestroy : true,
+									aaData : tableData,
+									aoColumns : [ {
+										mData : 'TokenID'
+									}, {
+										mData : 'UserID'
+									} ]
+								});
+
+								// 토큰클릭
+								$('#dataTable_Token tbody')
+										.on(
+												'click',
+												'tr',
+												function() {
+
+													var tableData = $(this)
+															.children("td")
+															.map(
+																	function() {
+																		return $(
+																				this)
+																				.text();
+																	}).get();
+
+													var token = tableData[0];
+													var hiddenUserID = tableData[1];
+													$('#hiddenUserID').val(
+															hiddenUserID);
+													$('#h3_tokenid')
+															.val(
+																	token
+																			+ "("
+																			+ hiddenUserID
+																			+ ")");
+													console.log(token);
+													if (token === 'No data available in table') {
+
+													} else {
+														$('#amdin-token-status')
+																.show();
+													}
+												});
+
+							} else {
+								alert('토큰 정보를 가지고 오는데 실패 하였습니다.');
+							}
+						},
+						error : function(data, textStatus, request) {
+							console.log(data);
+							alert('토큰 정보를 가지고 오는데 실패 하였습니다.');
 						}
+					});
 
-						// 기존 data 삭제
-
-						if (token_Table != null && token_Table != "") {
-							console.log('testconsole');
-							$('#dataTable_Token').children().remove();
-						}
-
-						token_Table = $('#dataTable_Token').dataTable({
-							bJQueryUI : true,
-							bDestroy : true,
-							aaData : tableData,
-							aoColumns : [ {
-								mData : 'TokenID'
-							}, {
-								mData : 'UserID'
-							} ]
-						});
-
-						// 토큰클릭
-						// $('#dataTable_Token tbody')
-						// .on(
-						// 'click',
-						// 'tr',
-						// function() {
-						//
-						// var tableData = $(this)
-						// .children("td")
-						// .map(
-						// function() {
-						// return $(
-						// this)
-						// .text();
-						// }).get();
-						//
-						//										
-						// var token = tableData[0];
-						// var hiddenUserID = tableData[1];
-						// $('#hiddenUserID').val(
-						// hiddenUserID);
-						// $('#h3_tokenid').val(
-						// token + "("
-						// + hiddenUserID
-						// + ")");
-						// $
-						// .ajax({
-						// url : '/v1/clients/'
-						// + token,
-						// type : 'GET',
-						// headers : {
-						// 'X-ApiKey' : tokenID
-						// },
-						// contentType : "application/json",
-						// async : false,
-						// success : function(
-						// data) {
-						// var tableData = [];
-						// if (data.result.data) {
-						//
-						// $(
-						// '#h2_tokenstatus')
-						// .val(
-						// data.result.data.status);
-						// } else {
-						// alert('토큰 정보를 가지고 오는데 실패 하였습니다.');
-						// wrapperFunction('tokenManager');
-						// }
-						// },
-						//														error : function(
-						//																data,
-						//																textStatus,
-						//																request) {
-						//
-						//															alert('토큰 정보를 가지고 오는데 실패 하였습니다.');
-						//															wrapperFunction('tokenManager');
-						//														}
-						//													});
-						//
-						//										});
-
-					} else {
-						alert('토큰 정보를 가지고 오는데 실패 하였습니다.');
-					}
-				},
-				error : function(data, textStatus, request) {
-					console.log(data);
-					alert('토큰 정보를 가지고 오는데 실패 하였습니다.');
-				}
-			});
-			
-		}else {
+		} else {
 			$
-			.ajax({
-				url : '/v1/tokens/ufmi/' + input_subscribe,
-				type : 'GET',
-				headers : {
-					'X-ApiKey' : tokenID
-				},
-				contentType : "application/json",
-				async : false,
-				success : function(data) {
-					var tableData = [];
-					if (data.result.data) {
+					.ajax({
+						url : '/v1/tokens/ufmi/' + input_subscribe,
+						type : 'GET',
+						headers : {
+							'X-ApiKey' : tokenID
+						},
+						contentType : "application/json",
+						async : false,
+						success : function(data) {
+							var tableData = [];
+							if (data.result.data) {
+								$('#amdin-token-list').show();
+								for ( var i in data.result.data) {
 
-						for ( var i in data.result.data) {
+									var item = data.result.data[i];
+									console.log(item);
+									tableData.push({
+										"TokenID" : item.tokenID,
+										"ID" : item.ufmi
+									});
+								}
 
-							var item = data.result.data[i];
-							console.log(item);
-							tableData.push({
-								"TokenID" : item.tokenID,
-								"ID" : item.ufmi
-							});
+								// 기존 data 삭제
+
+								if (token_Table != null && token_Table != "") {
+									console.log('testconsole');
+									$('#dataTable_Token').children().remove();
+								}
+
+								token_Table = $('#dataTable_Token').dataTable({
+									bJQueryUI : true,
+									bDestroy : true,
+									aaData : tableData,
+									aoColumns : [ {
+										mData : 'TokenID'
+									}, {
+										mData : 'ID'
+									} ]
+								});
+								// 토큰클릭
+								$('#dataTable_Token tbody')
+										.on(
+												'click',
+												'tr',
+												function() {
+
+													var tableData = $(this)
+															.children("td")
+															.map(
+																	function() {
+																		return $(
+																				this)
+																				.text();
+																	}).get();
+
+													var token = tableData[0];
+													var hiddenUserID = tableData[1];
+													$('#hiddenUserID').val(
+															hiddenUserID);
+													$('#h3_tokenid')
+															.val(
+																	token
+																			+ "("
+																			+ hiddenUserID
+																			+ ")");
+													console.log(token);
+													if (token === 'No data available in table') {
+
+													} else {
+														$('#amdin-token-status')
+																.show();
+													}
+												});
+								// 토큰클릭
+								// $('#dataTable_Token tbody')
+								// .on(
+								// 'click',
+								// 'tr',
+								// function() {
+								//
+								// var tableData = $(this)
+								// .children("td")
+								// .map(
+								// function() {
+								// return $(
+								// this)
+								// .text();
+								// }).get();
+								//
+								//										
+								// var token = tableData[0];
+								// var hiddenUserID = tableData[1];
+								// $('#hiddenUserID').val(
+								// hiddenUserID);
+								// $('#h3_tokenid').val(
+								// token + "("
+								// + hiddenUserID
+								// + ")");
+								// $
+								// .ajax({
+								// url : '/v1/clients/'
+								// + token,
+								// type : 'GET',
+								// headers : {
+								// 'X-ApiKey' : tokenID
+								// },
+								// contentType : "application/json",
+								// async : false,
+								// success : function(
+								// data) {
+								// var tableData = [];
+								// if (data.result.data) {
+								//
+								// $(
+								// '#h2_tokenstatus')
+								// .val(
+								// data.result.data.status);
+								// } else {
+								// alert('토큰 정보를 가지고 오는데 실패 하였습니다.');
+								// wrapperFunction('tokenManager');
+								// }
+								// },
+								// error : function(
+								// data,
+								// textStatus,
+								// request) {
+								//
+								// alert('토큰 정보를 가지고 오는데 실패 하였습니다.');
+								// wrapperFunction('tokenManager');
+								// }
+								// });
+								//
+								// });
+
+							} else {
+								alert('토큰 정보를 가지고 오는데 실패 하였습니다.');
+							}
+						},
+						error : function(data, textStatus, request) {
+							console.log(data);
+							alert('토큰 정보를 가지고 오는데 실패 하였습니다.');
 						}
+					});
 
-						// 기존 data 삭제
-
-						if (token_Table != null && token_Table != "") {
-							console.log('testconsole');
-							$('#dataTable_Token').children().remove();
-						}
-
-						token_Table = $('#dataTable_Token').dataTable({
-							bJQueryUI : true,
-							bDestroy : true,
-							aaData : tableData,
-							aoColumns : [ {
-								mData : 'TokenID'
-							}, {
-								mData : 'ID'
-							} ]
-						});
-
-						// 토큰클릭
-						// $('#dataTable_Token tbody')
-						// .on(
-						// 'click',
-						// 'tr',
-						// function() {
-						//
-						// var tableData = $(this)
-						// .children("td")
-						// .map(
-						// function() {
-						// return $(
-						// this)
-						// .text();
-						// }).get();
-						//
-						//										
-						// var token = tableData[0];
-						// var hiddenUserID = tableData[1];
-						// $('#hiddenUserID').val(
-						// hiddenUserID);
-						// $('#h3_tokenid').val(
-						// token + "("
-						// + hiddenUserID
-						// + ")");
-						// $
-						// .ajax({
-						// url : '/v1/clients/'
-						// + token,
-						// type : 'GET',
-						// headers : {
-						// 'X-ApiKey' : tokenID
-						// },
-						// contentType : "application/json",
-						// async : false,
-						// success : function(
-						// data) {
-						// var tableData = [];
-						// if (data.result.data) {
-						//
-						// $(
-						// '#h2_tokenstatus')
-						// .val(
-						// data.result.data.status);
-						// } else {
-						// alert('토큰 정보를 가지고 오는데 실패 하였습니다.');
-						// wrapperFunction('tokenManager');
-						// }
-						// },
-						// error : function(
-						// data,
-						// textStatus,
-						// request) {
-						//
-						// alert('토큰 정보를 가지고 오는데 실패 하였습니다.');
-						// wrapperFunction('tokenManager');
-						// }
-						// });
-						//
-						// });
-
-					} else {
-						alert('토큰 정보를 가지고 오는데 실패 하였습니다.');
-					}
-				},
-				error : function(data, textStatus, request) {
-					console.log(data);
-					alert('토큰 정보를 가지고 오는데 실패 하였습니다.');
-				}
-			});
-			
-			
-			
 		}
-		
-
 
 	}
 
@@ -314,12 +317,16 @@ function tokenDelete() {
 function tokenSubscribe() {
 	var formCheck = tokenIDFormCheck();
 	var h3_tokenid = $('#h3_tokenid').val();
+	var mq_list = $('#admin-mq-list-select').val();
 	var tokenIdArr = [];
+	mq_list = mq_list.split(':');
+
 	tokenIdArr = h3_tokenid.split('(');
 	if (formCheck) {
 
 		$.ajax({
-			url : '/v1/subscriptions/' + tokenIdArr[0],
+			url : '/v1/admin/subscriptions/' + tokenIdArr[0] + "?host="
+					+ mq_list[0] + "&port=" + mq_list[1],
 			type : 'GET',
 			headers : {
 				'X-ApiKey' : tokenID
@@ -328,9 +335,15 @@ function tokenSubscribe() {
 			async : false,
 			success : function(data) {
 				var tableData = [];
-				
+				console.log(data);
 				if (data.result.data) {
+					if (data.result.info) {
+						alert('subscription 정보가 없습니다.');
+						return;
+						// wrapperFunction('tokenManager');
+					}
 
+					$('#amdin-token-subscriptions').show();
 					for ( var i in data.result.data) {
 
 						var item = data.result.data[i];
@@ -351,28 +364,66 @@ function tokenSubscribe() {
 					});
 				}
 
-				else if (data.result.info) {
-					alert('subscription 정보가 없습니다.');
-					// wrapperFunction('tokenManager');
-					tableData.push({
-						"Topic" : "subscription 정보가 없습니다.",
-					});
-					$('#dataTable_Topic').dataTable({
-						bJQueryUI : true,
-						bDestroy : true,
-						aaData : tableData,
-						aoColumns : [ {
-							mData : 'Topic'
-						} ]
-					});
-				} else {
-					alert('subscription 정보를 가지고 오는데 실패 하였습니다.');
-					wrapperFunction('tokenManager');
+				else {
+					alert('MQ서버가 동작중이지 않거나 연결 할 수 없습니다. ');
+
 				}
 			},
 			error : function(data, textStatus, request) {
 				console.log(data);
-				alert('subscription 정보를 가지고 오는데 실패 하였습니다.');
+				alert('서버문제 정보를 가지고 오는데 실패 하였습니다.');
+				wrapperFunction('tokenManager');
+			}
+		});
+
+	}
+}
+
+function tokenConnectStatus() {
+	var formCheck = tokenIDFormCheck();
+	var h3_tokenid = $('#h3_tokenid').val();
+	var mq_list = $('#admin-mq-list-select').val();
+	var tokenIdArr = [];
+	mq_list = mq_list.split(':');
+
+	tokenIdArr = h3_tokenid.split('(');
+	if (formCheck) {
+
+		$.ajax({
+			url : '/v1/admin/clients/' + tokenIdArr[0] + "?host=" + mq_list[0]
+					+ "&port=" + mq_list[1],
+			type : 'GET',
+			headers : {
+				'X-ApiKey' : tokenID
+			},
+			contentType : "application/json",
+			async : false,
+			success : function(data) {
+				var tableData = [];
+				console.log(data);
+				if (data.result.data) {
+
+					$('#amdin-token-connect-status').show();
+					for ( var i in data.result.data) {
+
+						var item = data.result.data[i];
+						console.log(item);
+						$("#admin-connect-list-ul").append(
+								'<li class="list-group-item">' + mq_list[0]
+										+ ':' + mq_list[1] + '   ' + item
+										+ '</li>');
+					}
+
+				}
+
+				else {
+					alert('MQ서버가 동작중이지 않거나 연결 할 수 없습니다. ');
+
+				}
+			},
+			error : function(data, textStatus, request) {
+				console.log(data);
+				alert('서버문제 정보를 가지고 오는데 실패 하였습니다.');
 				wrapperFunction('tokenManager');
 			}
 		});
@@ -397,7 +448,14 @@ function formCheck() {
 
 function tokenIDFormCheck() {
 	var h3_tokenid = $('#h3_tokenid').val();
-
+	var mq_list = $('#admin-mq-list-select').val();
+	mq_list = mq_list * 1;
+	switch (mq_list) {
+	case 0:
+		alert('조회대상 MQ를 선택해 주세요');
+		return false;
+		break;
+	}
 	if (h3_tokenid == "" || h3_tokenid == null) {
 
 		alert('토큰 대상을 입력해 주세요');
